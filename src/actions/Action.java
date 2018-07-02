6224dc2272fd4e6a6b7e5107e07df7bb63fc9185
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2018-07-02 13:16:41 +0200

    Update action package javadoc

--- src/actions/Action.java
+++ src/actions/Action.java
@@ -5,16 +5,40 @@ import exceptions.ServerException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/**
+ * This class provide a base implementation for an action.
+ * <p>
+ * Since each class communicate with server,
+ * the class constructor provides through constructor injection
+ * the IO streams needed to send and receive data from server.
+ * </p>
+ * <p>
+ * The execute method must be defined in child classes and provides
+ * the actual logic of the action.
+ * </p>
+ */
 public abstract class Action {
 
     protected ObjectInputStream in;
     protected ObjectOutputStream out;
 
+    /**
+     * Construct the Action instance with IO streams.
+     *
+     * @param in input stream obtained from associated socket
+     * @param out output stream obtained from associated socket
+     */
     protected Action(final ObjectInputStream in,
                      final ObjectOutputStream out) {
         this.in = in;
         this.out = out;
     }
 
+    /**
+     * Execute the current action.
+     *
+     * @return Server response encoded as string
+     * @throws ServerException when response message have an error status.
+     */
     public abstract String execute() throws ServerException;
 }
---
+++ src/actions/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains classes to use with UI events.
+ *
+ * Each new action class should extends the Action abstract class
+ * that provide a base implementation common to every action
+ *
+ * Each action is responsible to handle client-server communication
+ * using {@link protocol} classes.
+ */
+package actions;
